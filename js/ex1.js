e1bcf4000a0d0c3c4e46c957e5d7aac503d5397f
Hang Le <hang.le01@student.csulb.edu>
2022-09-09 17:45:11 -0700

    js1 release

--- js/ex1.js
+++ js/ex1.js
@@ -3,3 +3,13 @@
 */
 
 console.log("Le's Output from HW3 Example 1");
+
+const number = prompt('Enter a number between 1 - 100');
+if (number>=1 && number<=100) {
+  alert(`Thank you! You entered ${number}, a valid number`);
+}
+else {
+  if ((number<1 || number>100) || isNaN(number)) {
+    alert(`Sorry, ${number} is not a valid entry`);
+  }
+}
