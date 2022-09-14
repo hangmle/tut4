a1c102129c1db9e69b4bed43dd7d079caf07723f
Hang Le <hang.le01@student.csulb.edu>
2022-09-14 14:32:35 -0700

    js5 changes

--- js/ex5.js
+++ js/ex5.js
@@ -7,11 +7,15 @@ console.log("Le's Output from HW3 Example 5");
 var pwd = prompt('Please enter your password');
 // let i=1;
 
-for (let i = 0; i <= 3; i) {
-  if (pwd == "") {
-    i++;
+for (let i = 0; i < 3; i) {
+  if (pwd !== "secret") {
     var pwd = prompt('Enter your password again');
-    console.log(i);
+    i++;
+    if (i == 3) {
+      console.log(`Your account is locked! You failed to enter the correct password ${i+1} times`);
+    }
   }
-  
+  if (pwd == "secret") {
+    console.log(`You entered the correct password after ${i} attempt(s)`)
+  }
 }
