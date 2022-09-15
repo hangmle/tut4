398c10d12b47058b86bfef894a0511ad41987d11
Hang Le <hang.le01@student.csulb.edu>
2022-09-15 02:32:03 -0700

    done2

--- js/ex2.js
+++ js/ex2.js
@@ -1,5 +1,5 @@
 /* HW3
-   Example 2 JavaScript code
+  Example 2 JavaScript code
 */
 
 console.log("Le's Output from HW3 Example 2");
@@ -7,21 +7,22 @@ console.log("Le's Output from HW3 Example 2");
 const userName = prompt('Please type your name');
 let unit = Number(prompt('How many units you have completed at college?'));
 
-while ((Number.isInteger(unit) !== true) || (unit < 0)) {
-  unit = prompt(`Please input an appropriate number`);
-  if ((Number.isInteger(unit) === true) && (unit >= 0)) {
-    console.log(`Name: ${userName}, have completed ${unit} units`);
-    alert(`
-      Hello ${userName}
-      Your grade standing is ${unit}
 
-      ${userName} - This is the name entered
-      ${unit} - Calculate as follows:
-      Number of units:
-      0 – 30: Freshman
-      31 – 60: Sophomore
-      61 – 90: Junior
-      > 91: Senior
-    `)
-  }
+if ((Number.isInteger(unit) !== true) || (unit < 0)) {
+  alert('Not an appropriate number. Start again')
+}
+if ((Number.isInteger(unit) === true) && (unit >= 0)) {
+  console.log(`Name: ${userName}, have completed ${unit} units`);
+  alert(`
+    Hello ${userName}
+    Your grade standing is ${unit}
+
+    ${userName} - This is the name entered
+    ${unit} - Calculate as follows:
+    Number of units:
+    0 – 30: Freshman
+    31 – 60: Sophomore
+    61 – 90: Junior
+    > 91: Senior
+  `)
 }
